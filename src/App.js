f21bbd5a0234031bbd067ab1872e1c2618dffd2b
VeronikaRiabysheva <ryabysheva01@inbox.ru>
2025-02-03 22:49:56 +0200

    styles changed

--- src/App.js
+++ src/App.js
@@ -36,10 +36,15 @@ function App() {
         )}
       </nav>
       <Routes>
-        <Route path="/" element={<Home isAuth={isAuth} />} />
+        <Route path="" element={<Home isAuth={isAuth} />} />
         <Route path="/createpost" element={<CreatePost isAuth={isAuth} />} />
         <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
+        <Route
+          path="/Blog-Life-style-notes"
+          element={<Home isAuth={isAuth} />}
+        />
       </Routes>
+
       <footer className="footer">
         <div>
           <p className="logoName">Life-Style-Notes</p>
